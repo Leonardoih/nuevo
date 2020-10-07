dd7b3adae959c8c1c53d0a9d594c6abdd712b069
leonardo <leonardoih090217@gmail.com>
2020-10-07 00:25:58 -0500

    cambio de Registro  a Registros

--- Registro.php
+++ Registro.php
@@ -80,7 +80,7 @@ if (
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Registro </title>
+    <title> Registros </title>
 </head>
 
 <div class="container">
